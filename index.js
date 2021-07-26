5e4217426c5ef3116eb761718909b363ff5e6496
mrandolph83 <mrandolph83@gmail.com>
2021-07-26 01:09:24 -0400

    commit tree ccc014bfbb0db2860311b74a1ccd9869f520528f

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Cat {
+    constructor(name){
+    this.name = name; 
+    
+}
+speak() {
+    return `${this.name} says meow!`
+}
+}
+
+class Dog {
+    constructor(name){
+        this.name = name
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex){
+    this.name = name 
+    this.sex = sex
+    }
+    speak() {
+       if (this.sex === "male") {
+           return `It's me! ${this.name}, the parrot!`
+       }
+       else {
+        return `${this.name} says squawk!`
+       }
+
+    }
+}
+
+// For each class, create the method speak.
+
+// For an instance of Cat, speak returns "name says meow!",
+// For an instance of Dog, speak returns "name says woof!"
+// For an instance of Bird, speak returns conditional output. 
+// If the instance of Bird is male, speak 
+// returns "It's me! name, the parrot!". If it is not male, 
+// speak returns "name says squawk!".
